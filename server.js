3908f1c575f9ab9c2fb2378c14ae5fd565c2735e
natalie <natalie-hasselmann@web.de>
2021-11-25 09:04:00 +0100

    fetch dat from cat api and destructure object

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const fetch = require('node-fetch')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+/**
+ * weather api call
+ * console.log data
+ * express server 
+ * server side rendering with pug
+ */
+
+function cat(req, res) {
+    fetch("https://cat-fact.herokuapp.com/facts")
+    .then(res => res.json())
+    //.then(json => console.log(json))
+    .then(json => res.send(json.map(({ text }) => ({ text }))))
+}
+app.get('/', cat)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
