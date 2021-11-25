d129024aa0605dec13afcb97ff7cac2caacf85b3
Natalie Hasselmann <natalie-hasselmann@web.de>
2021-11-25 12:02:46 +0100

    added request params

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const fetch = require('node-fetch')
 const express = require('express')
+require('dotenv').config();
 const app = express()
 const port = 3000
 
-/**
- * weather api call
- * console.log data
- * express server 
- * server side rendering with pug
- */
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
 
+// fetches data from open cat fact api, just for testing purposes
 function cat(req, res) {
     fetch("https://cat-fact.herokuapp.com/facts")
     .then(res => res.json())
@@ -17,15 +15,27 @@ function cat(req, res) {
     .then(json => res.send(json.map(({ text }) => ({ text }))))
 }
 
+// gets weather data from a certain location
 function weather(req, res) {
-    fetch("https://openweathermap.org/data/2.5/onecall?lat=50.7333&lon=7.1&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02")
+  const location = req.params.location;
+    fetch(`https://openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
     .then(res => res.json())
-    .then(json => res.send(json))
+    .then(json => res.send(json.weather))
 }
 
-app.get('/', cat)
-app.get('/weather', weather)
+//gets whole dataset from weather api
+function weather_all(req, res) {
+  fetch("https://openweathermap.org/data/2.5/weather?q=Bonn&appid=439d4b804bc8187953eb36d2a8c26a02")
+  .then(res => res.json())
+  .then(json => res.send(json))
+}
 
+//local routes
+app.get('/', cat)
+app.get('/weather/:location', weather)
+app.get('weatherAll', weather_all)
+
+//start express server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
