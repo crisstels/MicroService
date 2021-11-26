d8e37a19236231de62015be9b867ddf83fc8baeb
natalie <natalie-hasselmann@web.de>
2021-11-26 10:34:13 +0100

    added error page, new routes

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const fetch = require('node-fetch')
 const express = require('express')
+const path = require('path')
 require('dotenv').config();
 const app = express()
 const port = 3000
 
 const apiKey = process.env.API_KEY;
-console.log(apiKey);
 
 // fetches data from open cat fact api, just for testing purposes
-function cat(req, res) {
-    fetch("https://cat-fact.herokuapp.com/facts")
-    .then(res => res.json())
-    //.then(json => console.log(json))
-    .then(json => res.send(json.map(({ text }) => ({ text }))))
+function home(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
 }
 
 // gets weather data from a certain location
@@ -23,17 +20,28 @@ function weather(req, res) {
     .then(json => res.send(json.weather))
 }
 
-//gets whole dataset from weather api
+//gets whole dataset for a certain city from weather api
 function weather_all(req, res) {
-  fetch("https://openweathermap.org/data/2.5/weather?q=Bonn&appid=439d4b804bc8187953eb36d2a8c26a02")
-  .then(res => res.json())
-  .then(json => res.send(json))
+    const location = req.params.location;
+    console.log("location: " + location);
+    fetch(`https://openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(json => res.send(json))
+}
+
+//fetches temperature for specific city
+function temperature(req, res) {
+    const location = req.params.location;
+    fetch(`https://openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(json => res.send(json.main));
 }
 
 //local routes
-app.get('/', cat)
+app.get('/', home)
 app.get('/weather/:location', weather)
-app.get('weatherAll', weather_all)
+app.get('/:location', weather_all)
+app.get('/temp/:location', temperature);
 
 //start express server
 app.listen(port, () => {
